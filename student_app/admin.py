f008fe2ba6a95baf7032d29feebd944112971517
Gaurav Pandey <pandeyaayush7788@gmail.com>
2025-04-27 19:05:43 +0530

    Update all

--- student_app/admin.py
+++ student_app/admin.py
@@ -5,12 +5,3 @@ admin.site.register(Feedback)
 admin.site.register(Contact)
 admin.site.register(Subject)
 admin.site.register(UserProfile)
-class CustomAdminSite(admin.AdminSite):
-    class Media:
-        css = {
-            'all': ('css/admin_custom.css',)  # path jo static folder ke andar hai
-        }
-        js = ()  # Agar custom JavaScript bhi add karna ho to yaha likh sakte ho
-
-# Then use CustomAdminSite instead of default admin.site
-admin_site = CustomAdminSite(name='custom_admin')
